78e75f22dacb96714116b2ff1a214a19f7642e13
ANKIT <kumarankit0327@gmail.com>
2023-01-30 15:41:58 +0530

    server ready

--- 1Backend/server.js
+++ 1Backend/server.js
@@ -10,7 +10,7 @@ const database_url = process.env.DATABASE_URL || 'mongodb://localhost:27017/Firs
 const connectDatabase = require('./db/connection');
 // const router = require("cd-express/plugin/router");
 
-// connection with database
+// connection with databases
 connectDatabase(database_url);
 //body parser
 app.use(express.json());
